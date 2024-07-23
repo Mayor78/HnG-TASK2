5e8e883d2e06c635e25156aa4e50dc718b2484aa
Abraham mayowa <Abrahammayowa78@gmail.com>
2024-07-23 20:13:27 +0100

    navbar add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
         <div className=''>
         
            
-           <div onClick={handleToggle} className="block md:hidden p-2 focus:outline-none">
+           <div onClick={handleToggle} className="block md:hidden text-white p-2 focus:outline-none">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={show ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}></path>
             </svg>

0d30c04ea31d2ff0d071be828bbb2af8b15f5cc7
Abraham mayowa <Abrahammayowa78@gmail.com>
2024-11-27 00:32:35 +0100

    loader done

---
+++ src/components/LoaderFrame.jsx
@@ -0,0 +1,24 @@
+
+const LoaderFrame = () => {
+  return (
+    
+    <div className="h-[450px]   w-[400px] bg-slate-500 rounded-lg">
+        <div className="h-[450px]  glow  w-[400px]  bg-slate-300">
+       
+
+       
+        <div className="bg-slate-600 flex  relative justify-center h-[280px] rounded-t-lg  w-400px]">
+            <div className="bg-slate-400 w-[100px] absolute bottom-8 rounded-md  h-10">
+                
+            </div>
+        </div>
+        <div className="h-[30px] w-[90%] mx-2 rounded-2xl mt-3 bg-gray-500 "></div>
+        <div className="h-[70px] w-[90%] mx-2 rounded-2xl mt-3 bg-gray-500 "></div>
+        <div className="h-[15px] w-[20%] mx-2 rounded-2xl mt-3 bg-gray-500 "></div>
+        </div>
+        </div>
+    
+  )
+}
+
+export default LoaderFrame
